cec5a88f13cc0694c4066090d505b44298de8e12
with-pouyax <75066687+with-pouyax@users.noreply.github.com>
2024-12-01 19:45:35 +0100

    add commits until middle of load function

--- dictionary.c
+++ dictionary.c
@@ -18,8 +18,8 @@ typedef struct node
 // Number of buckets in hash table
 const unsigned int N = 100000; // Adjusted for better performance
 
-// Hash table
-node *table[N];
+
+node *table[N]; // table is an array of N pointers to nodes
 
 // Variable to keep track of the number of words loaded
 unsigned int word_count = 0;
@@ -27,69 +27,85 @@ unsigned int word_count = 0;
 // Hashes word to a number using djb2 algorithm
 unsigned int hash(const char *word)
 {
-    unsigned long hash = 5381;
-    int c;
+    unsigned long hash = 5381; // the hash value is initialized to 5381 as recommended by the djb2 algorithm
+    int c;                     // declare an integer variable to store the ASCII value of a character
 
-    while ((c = tolower(*word++)))
+    while ((c = tolower(*word++))) // loop through each character in the word and convert it to lowercase
     {
-        hash = ((hash << 5) + hash) + c; // hash * 33 + c
+        hash = ((hash << 5) + hash) + c; /* calculate the hash value using the djb2 algorithm
+                                            - the hash value is left-shifted by 5 bits and added to the hash value
+                                            - the ASCII value of the character is added to the hash value
+                                          */
     }
 
     return hash % N;
 }
 
-// Loads dictionary into memory, returning true if successful, else false
 bool load(const char *dictionary)
 {
-    // Initialize the hash table to NULL
-    for (unsigned int i = 0; i < N; i++)
+    for (unsigned int i = 0; i < N; i++) /*
+                                            - N is the number of buckets in the hash table, we set it to 100000 at the beginning of the program, we can adjust it for better performance
+                                            - we made an array of N pointers at the beginning of the program
+                                            - loop through each element in the table array
+                                         */
+        table[i] = NULL;                  // set each i'th element in the table array to NULL
+
+    FILE *file = fopen(dictionary, "r");  // open the dictionary file in read mode
+    if (file == NULL)                     // if the file could not be opened
     {
-        table[i] = NULL;
+        fprintf(stderr, "Could not open %s.\n", dictionary); // print an error message
+        return false;                                        // return false to indicate an error
     }
-
-    // Open the dictionary file
-    FILE *file = fopen(dictionary, "r");
-    if (file == NULL)
-    {
-        fprintf(stderr, "Could not open %s.\n", dictionary);
-        return false;
-    }
-
-    char word[LENGTH + 1];
-
+    char word[LENGTH + 1];                                   /* - make an array of LENGTH + 1 characters to store the word
+                                                                - LENGTH is the maximum length for a word, we set it to 45 at the beginning of the program
+                                                                - +1 is for the null terminator
+                                                             */
     // Read words from the file one at a time
-    while (fscanf(file, "%45s", word) != EOF)
+    while (fscanf(file, "%45s", word) != EOF)                /*
+                                                                - fscanf() reads formatted input from a file
+                                                                - it has 3 arguments: the file pointer, the format specifier, and the variable to store the input
+                                                                - file is the file pointer
+                                                                - %45s is the format specifier that reads a string of up to 45 characters
+                                                                - %45s means read a string of up to 45 characters or until a whitespace character is encountered, so
+                                                                - % means the beginning of the format specifier, 45 is the maximum number of characters to read, and s means read until a whitespace character is encountered.
+                                                                - word is the variable to store the word
+                                                                - fscanf(file, "%45s", word) reads a word of up to 45 characters from the file and stores it in the word variable
+                                                                - how the loop stops:
+                                                                - fscanf() returns EOF when it reaches the end of the file
+                                                                - the loop stops when fscanf() returns EOF
+                                                                - EOF is a constant defined in stdio.h that represents the end of the file
+                                                             */
     {
-        // Create a new node for each word
-        node *new_node = malloc(sizeof(node));
-        if (new_node == NULL)
+        node *new_node = malloc(sizeof(node));             // allocate memory for a new node to store the word
+        if (new_node == NULL)                              // if malloc() returns NULL
         {
-            fclose(file);
-            unload();
-            return false;
+            fclose(file);                                  // close the dictionary file
+            unload();                                      // unload the dictionary
+            return false;                                  // return false to indicate an error
+        }
+        strcpy(new_node->word, word);                      // copy the word into the new node
+        new_node->next = NULL;                             // set the next pointer of the new node to NULL
+
+        unsigned int index = hash(word);                   // hash the word to get the index
+
+        if (table[index] == NULL)                          // if the bucket is empty
+        {
+            table[index] = new_node;                      // insert the new node into the bucket
+        }
+        else                                              // if the bucket is not empty
+        {
+            new_node->next = table[index];                // set the next pointer of the new node to the current head of the linked list
+            table[index] = new_node;                      // set the new node as the head of the linked list
         }
 
-        // Copy the word into the node
-        strcpy(new_node->word, word);
-        new_node->next = NULL;
+        word_count++;                                    // increment the word count
+    }                                                    
 
-        // Hash the word to obtain a hash value
-        unsigned int index = hash(word);
-
-        // Insert the node into the hash table at the hashed index
-        if (table[index] == NULL)
-        {
-            table[index] = new_node;
-        }
-        else
-        {
-            new_node->next = table[index];
-            table[index] = new_node;
-        }
-
-        // Increment the word count
-        word_count++;
-    }
+                                                        /*
+                                                          - so in summery, in this while loop:
+                                                          - we read a word from the dictionary file
+                            
+                                                        */
 
     // Close the dictionary file
     fclose(file);
--- speller.c
+++ speller.c
@@ -12,185 +12,188 @@
 #undef getrusage
 
 // Default dictionary
-#define DICTIONARY "dictionaries/large"
+#define DICTIONARY "dictionaries/large" // the default dictionary is large
 
 // Prototype
 double calculate(const struct rusage *b, const struct rusage *a);
 
 int main(int argc, char *argv[])
 {
-    // Check for correct number of args
-    if (argc != 2 && argc != 3)
+                            /*
+                             - here in this program, we read 2 or 3 arguments from the command line
+                             - argv[0] is the name of the program
+                             - argv[1] is the name of the dictionary file
+                             - argv[2] is the name of the text file
+                             - the goal of the program is to check text file for misspelled words using the dictionary file and report the number of misspelled words
+                            */
+    struct rusage before;
+    struct rusage after;
+    double time_load;
+    double time_check;
+    double time_size;
+    double time_unload;
+    char *dictionary;
+
+    if (argc != 2 && argc != 3) // if the number of arguments is not 2 or 3
     {
         printf("Usage: ./speller [DICTIONARY] text\n");
         return 1;
     }
+    time_load = 0.0;
+    time_check = 0.0;
+    time_size = 0.0;
+    time_unload = 0.0;
+    dictionary = (argc == 3) ? argv[1] : DICTIONARY; // if the number of arguments is 3, set dictionary to argv[1], else set it to DICTIONARY
+    getrusage(RUSAGE_SELF, &before); /* 
+                                       - getrusage() is a system call that gets the resource usage of the calling process
+                                       - system call means that it is a request for the operating system to do something on behalf of the process
+                                       - resource usage means the amount of resources that the process is using
+                                       - getrusage() receives two arguments: the first is the resource to be queried, and the second is a pointer to a struct rusage
+                                       - RUSAGE_SELF is a macro that specifies that the resource usage of the calling process should be queried
+                                       - queried means that the resource usage is being asked for.
+                                       - RUSAGE_SELF is a constant that is used to specify the resource usage of the calling process
+                                       - &before is the address of the struct that will store the resource usage before the operation
+                                       - by before the operation, it means before the operation of loading the dictionary
+                                       -getrusage(RUSAGE_SELF, &before) gets the resource usage of the calling process before the operation of loading the dictionary and stores it in the struct before
+                                     */
 
-    // Structures for timing data
-    struct rusage before, after;
-
-    // Benchmarks
-    double time_load = 0.0, time_check = 0.0, time_size = 0.0, time_unload = 0.0;
-
-    // Determine dictionary to use
-    char *dictionary = (argc == 3) ? argv[1] : DICTIONARY;
-
-    // Load dictionary
-    getrusage(RUSAGE_SELF, &before);
-    bool loaded = load(dictionary);
-    getrusage(RUSAGE_SELF, &after);
-
-    // Exit if dictionary not loaded
-    if (!loaded)
+    bool loaded = load(dictionary); /*
+                                        - load() is a function that loads a dictionary into memory
+                                        - dictionary is a pointer to a string that contains the path to the dictionary file
+                                        - load(dictionary) loads the dictionary file into memory
+                                        - loaded is a boolean variable that stores whether the dictionary was loaded successfully
+                                        - if the dictionary was loaded successfully, loaded is set to true, else it is set to false
+                                    */
+    getrusage(RUSAGE_SELF, &after); // now we get the resource usage of the calling process after the operation of loading the dictionary and store it in the struct after
+    if (!loaded)                    // if the dictionary was not loaded successfully
     {
-        printf("Could not load %s.\n", dictionary);
-        return 1;
+        printf("Could not load %s.\n", dictionary); // print an error message
+        return 1;                                   // return 1 to indicate an error
     }
-
-    // Calculate time to load dictionary
-    time_load = calculate(&before, &after);
-
-    // Try to open text
-    char *text = (argc == 3) ? argv[2] : argv[1];
-    FILE *file = fopen(text, "r");
-    if (file == NULL)
+    time_load = calculate(&before, &after);         /*
+                                                      - calculate() is a function that calculates the time between two resource usage structs
+                                                      - it receives two arguments: the resource usage struct before the operation and the resource usage struct after the operation
+                                                      - it returns the number of seconds between the two resource usage structs
+                                                      - time_load is a double variable that stores the time it took to load the dictionary
+                                                      - calculate(&before, &after) calculates the time it took to load the dictionary and stores it in time_load
+                                                    */
+    char *text = (argc == 3) ? argv[2] : argv[1];   // if the number of arguments is 3, set text to argv[2], else set it to argv[1]
+    FILE *file = fopen(text, "r");                  // open the text file in read mode and store the file pointer in file
+    if (file == NULL)                              // if the file could not be opened
     {
-        printf("Could not open %s.\n", text);
-        unload();
-        return 1;
+        printf("Could not open %s.\n", text);      // print an error message
+        unload();                                  /*
+                                                        - unload() is a function that unloads the dictionary from memory
+                                                        - it returns a boolean value that indicates whether the dictionary was unloaded successfully
+                                                        - unload() unloads the dictionary from memory
+                                                        - if we do not unload the dictionary, we will have a memory leak
+                                                   */
+        return 1;                                   // return 1 to indicate an error
     }
+    printf("\nMISSPELLED WORDS\n\n");               // print a message to indicate that the misspelled words will be printed below
 
-    // Prepare to report misspellings
-    printf("\nMISSPELLED WORDS\n\n");
+    int     index = 0;
+    int     misspellings = 0;
+    int     words = 0;
+    char    word[LENGTH + 1];                        // +1 for null terminator
 
-    // Prepare to spell-check
-    int index = 0, misspellings = 0, words = 0;
-    char word[LENGTH + 1];
-
-    // Spell-check each word in text
-    char c;
-    while (fread(&c, sizeof(char), 1, file))
+    char c;                                          // character to read from file
+    while (fread(&c, sizeof(char), 1, file))         // read one character at a time from the file
     {
-        // Allow only alphabetical characters and apostrophes
-        if (isalpha(c) || (c == '\'' && index > 0))
+        if (isalpha(c) || (c == '\'' && index > 0))  // if the character is a letter or an apostrophe and the index is greater than 0
         {
-            // Append character to word
-            word[index] = c;
-            index++;
-
-            // Ignore alphabetical strings too long to be words
-            if (index > LENGTH)
+            word[index] = c;                         // store the character in the word array
+            index++;                                 // increment the index
+            if (index > LENGTH)                      // if the index is greater than the maximum length of a word
             {
-                // Consume remainder of alphabetical string
-                while (fread(&c, sizeof(char), 1, file) && isalpha(c));
+                while (fread(&c, sizeof(char), 1, file) && isalpha(c)); /*
+                                                                            - while the character is a letter, read the next character
+                                                                            - this is to consume the rest of the word
+                                                                            - we do not want to check the spelling of a word that is longer than the maximum length
+                                                                            - we are ignoring words that are longer than the maximum length
+                                                                            - this is similar to how MS Word ignores words that are too long
+                                                                            - we dont have i++ here because we are not storing the characters in the word array
+                                                                            - the while loop will keep reading characters until it reaches a non-letter character
+                                                                        */
 
-                // Prepare for new word
-                index = 0;
+                //after consuming the rest of the word, we prepare for the next word
+                index = 0; // by setting the index to 0
             }
         }
-
-        // Ignore words with numbers (like MS Word can)
-        else if (isdigit(c))
+        else if (isdigit(c)) // if the character is a digit
         {
-            // Consume remainder of alphanumeric string
-            while (fread(&c, sizeof(char), 1, file) && isalnum(c));
-
-            // Prepare for new word
-            index = 0;
+            while (fread(&c, sizeof(char), 1, file) && isalnum(c)); // consume the rest of the word until we reach a non alphanumeric character
+                                                                    // alphanumeric means a letter or a digit
+            // we prepare for the next word
+            index = 0; // by setting the index to 0
         }
-
-        // We must have found a whole word
-        else if (index > 0)
+        else if (index > 0) // if the character is not a letter, digit, or apostrophe and the index is greater than 0
         {
-            // Terminate current word
-            word[index] = '\0';
-
-            // Update counter
-            words++;
-
-            // Check word's spelling
-            getrusage(RUSAGE_SELF, &before);
-            bool misspelled = !check(word);
-            getrusage(RUSAGE_SELF, &after);
-
-            // Update benchmark
-            time_check += calculate(&before, &after);
-
-            // Print word if misspelled
-            if (misspelled)
+            // it means we have reached the end of a word
+            word[index] = '\0'; // so we add a null terminator to the word
+            words++;            // increment the number of words
+            getrusage(RUSAGE_SELF, &before); // get the resource usage before checking the spelling of the word  
+            bool misspelled = !check(word);  // check if the word is misspelled
+            getrusage(RUSAGE_SELF, &after);  // get the resource usage after checking the spelling of the word
+            time_check += calculate(&before, &after); // calculate the time it took to check the spelling of the word and add it to time_check
+            if (misspelled) // if the word is misspelled
             {
-                printf("%s\n", word);
-                misspellings++;
+                printf("%s\n", word); // print the word
+                misspellings++;      // increment the number of misspelled words
             }
-
             // Prepare for next word
-            index = 0;
+            index = 0; // by setting the index to 0
         }
     }
-
-    // Check whether there was an error
-    if (ferror(file))
+    if (ferror(file)) /*
+                        - ferror() is a function that checks if an error occurred while reading from a file
+                        - it returns a non-zero value if an error occurred, else it returns 0
+                      */
     {
-        fclose(file);
-        printf("Error reading %s.\n", text);
-        unload();
-        return 1;
+        fclose(file); // close the file
+        printf("Error reading %s.\n", text); // print an error message
+        unload(); // unload the dictionary
+        return 1; // return 1 to indicate an error
     }
-
-    // Close text
-    fclose(file);
-
-    // Determine dictionary's size
-    getrusage(RUSAGE_SELF, &before);
-    unsigned int n = size();
-    getrusage(RUSAGE_SELF, &after);
-
-    // Calculate time to determine dictionary's size
-    time_size = calculate(&before, &after);
-
-    // Unload dictionary
-    getrusage(RUSAGE_SELF, &before);
-    bool unloaded = unload();
-    getrusage(RUSAGE_SELF, &after);
-
-    // Abort if dictionary not unloaded
-    if (!unloaded)
+    fclose(file); // close the file
+    getrusage(RUSAGE_SELF, &before); // get the resource usage before determining the size of the dictionary
+    unsigned int n = size();         // determine the size of the dictionary and store it in n
+    getrusage(RUSAGE_SELF, &after);  // get the resource usage after determining the size of the dictionary
+    time_size = calculate(&before, &after);  // calculate the time it took to determine the size of the dictionary and store it in time_size
+    getrusage(RUSAGE_SELF, &before);         // get the resource usage before unloading the dictionary
+    bool unloaded = unload();                // unload the dictionary and store whether it was unloaded successfully in unloaded
+    getrusage(RUSAGE_SELF, &after);          // get the resource usage after unloading the dictionary
+    if (!unloaded)                         // if the dictionary was not unloaded successfully
     {
-        printf("Could not unload %s.\n", dictionary);
-        return 1;
+        printf("Could not unload %s.\n", dictionary); // print an error message
+        return 1; // return 1 to indicate an error
     }
+    time_unload = calculate(&before, &after);  // calculate the time it took to unload the dictionary and store it in time_unload
+    printf("\nWORDS MISSPELLED:     %d\n", misspellings); // print the number of misspelled words
+    printf("WORDS IN DICTIONARY:  %d\n", n);              // print the number of words in the dictionary
+    printf("WORDS IN TEXT:        %d\n", words);          // print the number of words in the text
+    printf("TIME IN load:         %.2f\n", time_load);    // print the time it took to load the dictionary
+    printf("TIME IN check:        %.2f\n", time_check);   // print the time it took to check the spelling of the words
+    printf("TIME IN size:         %.2f\n", time_size);    // print the time it took to determine the size of the dictionary
+    printf("TIME IN unload:       %.2f\n", time_unload);  // print the time it took to unload the dictionary
+    printf("TIME IN TOTAL:        %.2f\n\n",  
+           time_load + time_check + time_size + time_unload); // print the total time it took to load, check, determine the size, and unload the dictionary
 
-    // Calculate time to unload dictionary
-    time_unload = calculate(&before, &after);
-
-    // Report benchmarks
-    printf("\nWORDS MISSPELLED:     %d\n", misspellings);
-    printf("WORDS IN DICTIONARY:  %d\n", n);
-    printf("WORDS IN TEXT:        %d\n", words);
-    printf("TIME IN load:         %.2f\n", time_load);
-    printf("TIME IN check:        %.2f\n", time_check);
-    printf("TIME IN size:         %.2f\n", time_size);
-    printf("TIME IN unload:       %.2f\n", time_unload);
-    printf("TIME IN TOTAL:        %.2f\n\n",
-           time_load + time_check + time_size + time_unload);
-
-    // Success
-    return 0;
+    return 0; // return 0 to indicate success
 }
 
-// Returns number of seconds between b and a
 double calculate(const struct rusage *b, const struct rusage *a)
 {
-    if (b == NULL || a == NULL)
+    if (b == NULL || a == NULL) // if either of the resource usage structs is NULL
     {
-        return 0.0;
+        return 0.0; // return 0.0
     }
-    else
+    else // if both resource usage structs are not NULL
     {
         return ((((a->ru_utime.tv_sec * 1000000 + a->ru_utime.tv_usec) -
                   (b->ru_utime.tv_sec * 1000000 + b->ru_utime.tv_usec)) +
                  ((a->ru_stime.tv_sec * 1000000 + a->ru_stime.tv_usec) -
                   (b->ru_stime.tv_sec * 1000000 + b->ru_stime.tv_usec)))
-                / 1000000.0);
+                / 1000000.0); // calculate the time between the two resource usage structs and return it
     }
 }
